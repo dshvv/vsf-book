8e93ea092c6fa418fbe1ae3699d5671afbd0fd82
丁少华 <dingshaohua@aegis-data.cn>
2021-11-04 20:00:42 +0800

    feat: 优化代码

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -8,15 +8,9 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import SchemaForm, {registerAntd} from 'v-schema-form';
 
-registerAntd(Antd);
-
 export default defineClientAppEnhance(({ app, router, siteData }) => {
     console.log(12333, app);
     app.use(Antd);
     app.use(SchemaForm);
-
-    const languageVue = document.querySelectorAll('.language-vue');
-    languageVue.onclick=function(e){
-        console.log(e);
-    }
+    registerAntd(Antd);
 })
